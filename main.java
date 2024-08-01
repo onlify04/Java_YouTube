244151b4d31147cb29a3901317a58b32829bf67e
OnliFy <onlify04@gmail.com>
2024-08-01 20:09:52 +0530

    initial commit

---
+++ main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.print("This is ");
+        System.out.print("on the same line.");
+        
+        System.out.println("This is on a new line.");
+        System.out.println("This is also on a new line.");
+        
+        String name = "World";
+        System.out.printf("Hello, %s!", name);
+    }
+}
