12df30ab63bcec42e1a1f55df89b6972508f5f62
OnliFy <onlify04@gmail.com>
2024-08-11 20:39:34 +0530

    Latest commit Lecture 2

---
+++ Lecture_2.java
@@ -0,0 +1,33 @@
+public class Lecture_2 {
+    public static void main(String[] args) {
+        /*  int alok = 2;
+        int Alok = 2;
+        int aLok = 6;
+        int alokKumar$_585 = 548;
+        byte name = -52;
+        byte name2;
+        System.out.println(name);
+        short number = 25126;
+        short number2;
+        System.out.println(number);
+        int a = 5485575;
+        int a2;
+        System.out.println(a);
+        long x = 5475555587555856758L;
+        long x1 = 574558;
+        float y = 84755.22541582552F;
+        float y1;
+        System.out.println(y);
+        System.out.println(y1 = 5874.55844F);
+        double z = 574.5548575554415785D;
+        double z1;
+        System.out.println(z);
+        char channel = 'O';
+        char channel1;
+        System.out.println(channel);
+        boolean password = true;
+        boolean password1;
+        System.out.println(password);
+        */
+    }    
+}
