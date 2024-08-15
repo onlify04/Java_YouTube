ae8b3fd7972c65c0c2aec6bf9e585006aec721e4
OnliFy <onlify04@gmail.com>
2024-08-15 20:16:08 +0530

    Commit Lecture 4

---
+++ Lecture_3.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class Lecture_3 {
+    public static void main(String[] args) {
+        /*
+        // Key board input
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = s.nextInt();
+        System.out.print(number);
+        boolean name = s.nextBoolean();
+        System.out.println(name);
+        // boolean name = s.hasNextInt();
+        // System.out.println(name);
+        // 5 = 52  1 = 49
+        // Arithmetic Operator
+        char num1 = '5';
+        char num2 = '1';
+        int result = num1+num2;
+        System.out.println(result);
+        int a = 7;
+        int b = a%2;
+        System.out.println(b);
+        // Assignment Operator
+        int a = 20;
+        a += 6;
+        System.out.println(a);
+        // Logical Operator
+        int a = 2;
+        int b = 6;
+        boolean tell = a==b;
+        System.out.println(tell);
+        boolean tell1 = b!=a;
+        System.out.println(tell1);
+        // Bitwish Operators
+        System.out.println(2&3);.
+        // Precedence 
+        int a = 5;
+        int b = 6;
+        int c = 2;
+        int d = 10;
+        System.out.println(a*b+d);
+        System.out.println(a+b*d);
+        System.out.println(a*b/c);
+        System.out.println(a+b-c);
+        Increment and Decrement Operators
+        int a = 2;
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(a);
+        char sum = 'O';
+        sum++;
+        System.out.println(sum);
+        int b = 3;
+        System.out.println(b--);
+        System.out.println(--b);
+        System.out.println(b);
+        // result of data type on arithmetic operations
+        byte s = 52;
+        short d = 30;
+        // System.out.println(s+d);
+        int e = 10;
+        long f = 2551452555544445L;
+        float g = 548.25154F;
+        // System.out.println(d+e);
+        System.out.println(f+g);
+        */
+
+    }
+}
---
+++ Lecture_4.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+public class Lecture_4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        /*
+        // Question 1  (Type 1)
+        int a = 20;
+        int b = 12;
+        int c = 36;
+        int d = 1;
+        int e = 9;
+        int sum = a+b+c+d+e;
+        System.out.println("The Sum: " + sum);
+
+        float A = 20;
+        float B = 13;
+        float C = 18;
+        float D = 15.24F;
+        float E = 95.42f;
+        float Sum = A+B+C+D+E;
+        System.out.println("The float Sum: " + Sum);
+        // Question 2
+        System.out.print("Enter your Name: ");
+        String name = scanner.nextLine();
+        System.out.println("hello "+name+" !");
+
+        // Question 3
+
+        System.out.print("Enter the minutes: ");
+        int min = scanner.nextInt();
+        int sec = min*60;
+        System.out.println("Total seconds: "+sec+" seconds");
+
+        // Question 4
+
+        float a = 6/4*8/3;
+        System.out.println("Result: "+a);
+
+        // Question 6 
+
+        float y = 9;
+        float m = y/3-3*0+25*1/y;
+        System.out.println("result: "+m);
+
+        // Question 7
+
+        System.out.print("Enter marks of Maths: ");
+        int maths = scanner.nextInt();
+        System.out.print("Enter marks of English: ");
+        int english = scanner.nextInt();
+        System.out.print("Enter marks of Hindi: ");
+        int hindi = scanner.nextInt();
+        System.out.print("Enter marks of Computer: ");
+        int computer = scanner.nextInt();
+        System.out.print("Enter marks of Science: ");
+        int science = scanner.nextInt();
+        System.out.print("Enter marks of SSt: ");
+        int sst = scanner.nextInt();
+
+        float sum = maths+english+hindi+science+computer+sst;
+        // 6 * 80 = 480
+
+        float per = (sum/480)*100;
+        System.out.println("Your Percentange: " + per);
+
+        // Question 8
+        int m = 5;
+        int n = --m*2;
+        System.out.println(n);
+        */
+
+        // Question 9
+        int v = 5;
+        int u = 5;
+        int a = 5;
+        int s = 5;
+        int b = 5;
+        int x = 5;
+        int t = 5;
+        int c = 5;
+
+        int eq1 = (v*v)-(u*u)/(2*a*s);
+        int eq2 = (v-u)/2;
+        int eq3 = (u*t)+(1/2*(a*(t*t)));
+        int eq4 = ((b*b)-4*a*c)/2;
+        int eq5 = (x*x)-x+52;
+        
+    }
+}
